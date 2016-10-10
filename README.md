2548b6c4e99b0d94acd63d574c91f55609fa7dd4
David29595 <david-daly@live.ie>
2016-10-10 22:07:37 +0100

    Cleanup

--- README.md
+++ README.md
@@ -9,4 +9,11 @@ A Pomodorro Tracker application written in Angular JS. Uses the electron framewo
 ### Dev Usage
 `git clone https://github.com/David29595/Pomodorro-Tracker.git`
 <br/>`npm install`
-<br/>`npm dev` to start the app with developer feature enabled
+<br/>`gulp start` to start the app with developer features enabled
+
+###### Folder Structure:
+Develop code in the `src` folder. When the build process runs, it takes the code from the `src` directory and compiles and transfers it into the `app` directory.
+
+### Other useful commands
+`gulp build` build the application's source
+<br/>`gulp watch` create a watch oin the application's source that updates the bundle when the `src` directory changes
--- src/background.js
+++ src/background.js
@@ -4,32 +4,31 @@ import { app, Menu, BrowserWindow} from 'electron';
 import { devMenuTemplate } from './menu/dev_menu_template';
 import { editMenuTemplate } from './menu/edit_menu_template';
 
-// Special module holding environment variables which you declared
-// in config/env_xxx.json file.
 import env from './env';
 
-var mainWindow;
+var mainWindow, width, height;
 
-var setApplicationMenu = function () {
+var init = function () {
     var menus = [editMenuTemplate];
     if (env.name !== 'production') {
         menus.push(devMenuTemplate);
     }
     Menu.setApplicationMenu(Menu.buildFromTemplate(menus));
-};
+
+    width = env.name === 'development' ? 1000 : 450;
+    height = env.name === 'development' ? 800 : 500;
+
+    mainWindow = new BrowserWindow({width: width, height: height, frame: false, resizable: true});
+}
 
 app.on('ready', function () {
-    setApplicationMenu();
-
-    var mainWindow = new BrowserWindow({width: 450, height: 500, frame: false, resizable: true});
+    init();
 
     mainWindow.loadURL('file://' + __dirname + '/app.html');
 
     if (env.name === 'development') {
         mainWindow.openDevTools();
     }
-
-
 });
 
 app.on('window-all-closed', function () {
